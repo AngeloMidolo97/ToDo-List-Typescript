3a727b495cb010aeebb752c898f976025ca3bfa1
AngeloMidolo97 <angelo.midolo97@gmail.com>
2022-11-25 15:49:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,112 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var btnTask = document.getElementById('btn');
+var inpTask = document.getElementById('task');
+var list = document.getElementById('print');
+var btnDel = document.getElementById('btnDel');
+class Task {
+    constructor(_id, _task, _complete) {
+        this.id = _id,
+            this.task = _task,
+            this.complete = _complete;
+    }
+    completa() {
+        if (this.complete === true) {
+            this.complete = false;
+        }
+        else {
+            this.complete = true;
+        }
+        fetch('http://localhost:3000/todo/' + this.id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify(this)
+        });
+    }
+    delTask() {
+        if (!window.confirm('Sicuro di voler eliminare questo utente?')) {
+            return;
+        }
+        fetch('http://localhost:3000/todo/' + this.id, {
+            method: 'DELETE'
+        });
+    }
+    creaRiga() {
+        var newDiv = document.createElement('div');
+        var h2 = document.createElement('h2');
+        var newDiv2 = document.createElement('div');
+        var btn1 = document.createElement('button');
+        var btn2 = document.createElement('button');
+        list.appendChild(newDiv);
+        newDiv.appendChild(h2);
+        newDiv.appendChild(newDiv2);
+        newDiv2.appendChild(btn1);
+        newDiv2.appendChild(btn2);
+        newDiv.classList.add('container', 'mt-2', 'border', 'rounded', 'd-flex', 'justify-content-between', 'p-3');
+        btn1.classList.add('btn', 'btn-success', 'mx-1');
+        btn2.classList.add('btn', 'btn-primary');
+        h2.innerText = this.task;
+        btn1.innerHTML = '<i class="bi bi-check2-square">';
+        btn2.innerHTML = '<i class="bi bi-trash"></i>';
+        if (this.complete === true) {
+            h2.classList.add('complete');
+        }
+        else {
+            h2.classList.remove('complete');
+        }
+        btn1.addEventListener('click', () => {
+            this.completa();
+        });
+        btn2.addEventListener('click', () => {
+            this.delTask();
+        });
+    }
+}
+getData();
+let array;
+function getData() {
+    fetch('http://localhost:3000/todo').then((response) => {
+        return response.json();
+    }).then((data) => {
+        array = [];
+        array = data;
+        console.log(array);
+        array.map(function (element) {
+            let c = new Task(element.id, element.task, element.complete);
+            console.table(c);
+            c.creaRiga();
+        });
+    });
+}
+btnTask.addEventListener('click', () => {
+    var newTask = {
+        task: inpTask.value,
+        complete: false
+    };
+    addData(newTask);
+});
+function addData(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (inpTask.value === '') {
+            alert('Attenzione compilare il campo');
+            return;
+        }
+        let response = yield fetch('http://localhost:3000/todo', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify(data),
+        });
+    });
+}
---
+++ script.ts
@@ -0,0 +1,133 @@
+var btnTask = document.getElementById('btn') as HTMLButtonElement
+var inpTask = document.getElementById('task') as HTMLInputElement
+var list = document.getElementById('print') as HTMLElement
+var btnDel = document.getElementById('btnDel') as HTMLElement
+
+interface TaskInt {
+    id: number,
+    task: string,
+    complete: boolean
+}
+
+interface NewTask {
+    task: string,
+    complete: boolean
+}
+
+interface TaskId {
+    id: number
+}
+
+interface TaskComplete {
+    complete: boolean
+}
+
+class Task {
+    id: number;
+    task: string;
+    complete: boolean;
+    constructor(_id: number, _task: string, _complete: boolean) {
+        this.id = _id,
+            this.task = _task,
+            this.complete = _complete
+    }
+    completa() {
+        if(this.complete === true){
+            this.complete = false;
+        }else{
+            this.complete = true;
+        }
+        fetch('http://localhost:3000/todo/' + this.id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify(this)
+        });
+    }
+    delTask() {
+        if (!window.confirm('Sicuro di voler eliminare questo utente?')) {
+            return;
+        }
+        fetch('http://localhost:3000/todo/' + this.id, {
+            method: 'DELETE'
+        });
+    }
+    creaRiga() {
+        var newDiv = document.createElement('div');
+        var h2 = document.createElement('h2');
+        var newDiv2 = document.createElement('div');
+        var btn1 = document.createElement('button');
+        var btn2 = document.createElement('button');
+
+        list.appendChild(newDiv);
+        newDiv.appendChild(h2);
+        newDiv.appendChild(newDiv2);
+        newDiv2.appendChild(btn1);
+        newDiv2.appendChild(btn2);
+
+        newDiv.classList.add('container', 'mt-2', 'border', 'rounded', 'd-flex', 'justify-content-between', 'p-3');
+        btn1.classList.add('btn', 'btn-success', 'mx-1');
+        btn2.classList.add('btn', 'btn-primary');
+        
+
+        h2.innerText = this.task;
+        btn1.innerHTML = '<i class="bi bi-check2-square">';
+        btn2.innerHTML = '<i class="bi bi-trash"></i>';
+        if(this.complete === true){
+            h2.classList.add('complete')
+        }else{
+            h2.classList.remove('complete')
+        }
+
+        btn1.addEventListener('click', () => {
+            this.completa();
+        })
+
+        btn2.addEventListener('click', () => {
+            this.delTask();
+        })
+    }
+}
+
+getData();
+
+let array: TaskInt[];
+
+function getData() {
+    fetch('http://localhost:3000/todo').then((response) => {
+        return response.json();
+    }).then((data: TaskInt[]) => {
+        array = [];
+        array = data;
+        console.log(array);
+        array.map(function (element) {
+            let c = new Task(element.id, element.task, element.complete);
+            console.table(c);
+            c.creaRiga();
+        })
+    })
+}
+
+btnTask.addEventListener('click', () => {
+    var newTask = {
+        task: inpTask.value,
+        complete: false
+    }
+    addData(newTask);
+})
+
+async function addData(data: NewTask) {
+    if(inpTask.value === ''){
+        alert('Attenzione compilare il campo');
+        return;
+    }
+    let response = await fetch('http://localhost:3000/todo', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(data),
+    });
+}
+
